52d1b5630bed32eca14772be8d3bc9241892bb97
Melvin <80354503+cr7ronaldocom@users.noreply.github.com>
2022-04-16 13:40:07 -0500

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,13 @@
+function loadUrls() {
+  // fetch urls from textarea and load them
+  
+}
+
+function saveUrls() {
+  
+  // Fetch urls and save it in chrome storage
+}
+
+
+document.getElementById('button').addEventListener('click', loadUrls);
+document.getElementById('button').addEventListener('click', saveUrls);
